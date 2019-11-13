834a97beaae2b3125f31e2e0aa7e7f5452b1ea08
SAJID LATIF <indus1530@gmail.com>
2019-11-13 14:37:59 +0500

    4th Section savedraft

---
+++ app/src/main/java/edu/aku/hassannaqvi/rsvstudy/ui/form1/Section01Activity.java
@@ -0,0 +1,186 @@
+package edu.aku.hassannaqvi.rsvstudy.ui.form1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import edu.aku.hassannaqvi.rsvstudy.R;
+import edu.aku.hassannaqvi.rsvstudy.contracts.FormsContract;
+import edu.aku.hassannaqvi.rsvstudy.contracts.LHWContract;
+import edu.aku.hassannaqvi.rsvstudy.core.DatabaseHelper;
+import edu.aku.hassannaqvi.rsvstudy.core.MainApp;
+import edu.aku.hassannaqvi.rsvstudy.databinding.ActivityF1Section01Binding;
+import edu.aku.hassannaqvi.rsvstudy.validator.ValidatorClass;
+
+public class Section01Activity extends AppCompatActivity {
+
+    public static String DOB;
+    ActivityF1Section01Binding bi;
+    private List<String> ucName, talukaNames, villageNames, chwNames;
+    private List<String> ucCode, talukaCodes, villageCodes, chwCodes;
+    private DatabaseHelper db;
+    private String dtToday = new SimpleDateFormat("dd-MM-yy HH:mm").format(new Date().getTime());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_f1_section01);
+        bi.setCallback(this);
+        this.setTitle("RSV Study section 1");
+        //initializingComponents();
+
+    }
+
+    private void initializingComponents() {
+        db = new DatabaseHelper(this);
+        populateSpinner(this);
+
+        //bi.pocfa12.setMinDate(DateUtils.getYearsBack("dd/MM/yyyy", -5));
+    }
+
+    public void populateSpinner(final Context context) {
+        // Spinner Drop down elements
+        chwNames = new ArrayList<>();
+        chwCodes = new ArrayList<>();
+
+        chwNames.add("....");
+        chwCodes.add("....");
+
+        Collection<LHWContract> dc = db.getAllLhw();
+
+        for (LHWContract d : dc) {
+            chwNames.add(d.getLhwname());
+            chwCodes.add(d.getLhwcode());
+        }
+
+        bi.RS13.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, chwNames));
+
+        bi.RS13.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) return;
+                bi.chwcode.setText("LHW Code: " + chwCodes.get(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                finish();
+                startActivity(new Intent(this, Section02Activity.class));
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    public void BtnEnd() {
+
+        if (!ValidatorClass.EmptyCheckingContainer(this, bi.ll01))
+            return;
+
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (UpdateDB()) {
+            MainApp.endActivity(this, this);
+        } else {
+            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private boolean UpdateDB() {
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+        long updcount = db.addForm(MainApp.fc);
+
+        MainApp.fc.set_ID(String.valueOf(updcount));
+        if (updcount != 0) {
+            MainApp.fc.set_UID(
+                    (MainApp.fc.getDeviceID() + MainApp.fc.get_ID()));
+            db.updateFormID();
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private void SaveDraft() throws JSONException {
+
+        MainApp.fc = new FormsContract();
+        MainApp.fc.setDeviceID(MainApp.deviceId);
+        MainApp.fc.setAppversion(MainApp.versionName + "." + MainApp.versionCode);
+        MainApp.fc.setUser(MainApp.userName);
+        MainApp.fc.setFormDate(dtToday);
+        MainApp.fc.setDevicetagID(getSharedPreferences("tagName", MODE_PRIVATE).getString("tagName", ""));
+
+        MainApp.fc.setRef_ID(bi.RSID.getText().toString());
+        //MainApp.fc.setCode_lhw(chwCodes.get(bi.RS13.getSelectedItemPosition()));
+        //MainApp.fc.setRef_ID(bi.chwcode.getText().toString());
+
+        JSONObject SA = new JSONObject();
+
+        SA.put("RS7", bi.RS7.getText().toString());
+        SA.put("RS8", bi.RS8.getText().toString());
+        SA.put("RS9", bi.RS9.getText().toString());
+        SA.put("RS10", bi.RS10.getText().toString());
+        SA.put("RS11", bi.RS11.getText().toString());
+        SA.put("RS12", bi.RS12.getText().toString());
+
+        MainApp.fc.setsA(String.valueOf(SA));
+        MainApp.setGPS(this);
+
+        /*DOB = getDOB();*/
+
+    }
+
+
+    /*private String getDOB() {
+        if (bi.pocfa11a.isChecked())
+            return DateUtils.convertDateFormat(bi.pocfa12.getText().toString());
+        else return DateUtils.getDOB("dd/MM/yyyy",
+                Integer.valueOf(bi.pocfa13y.getText().toString()),
+                Integer.valueOf(bi.pocfa13m.getText().toString()),
+                Integer.valueOf(bi.pocfa13d.getText().toString()));
+    }*/
+
+    private boolean formValidation() {
+        return ValidatorClass.EmptyCheckingContainer(this, bi.ll01);
+    }
+
+}
+
---
+++ app/src/main/java/edu/aku/hassannaqvi/rsvstudy/ui/form1/Section04Activity.java
@@ -0,0 +1,188 @@
+package edu.aku.hassannaqvi.rsvstudy.ui.form1;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.aku.hassannaqvi.rsvstudy.R;
+import edu.aku.hassannaqvi.rsvstudy.core.DatabaseHelper;
+import edu.aku.hassannaqvi.rsvstudy.core.MainApp;
+import edu.aku.hassannaqvi.rsvstudy.databinding.ActivityF1Section04Binding;
+import edu.aku.hassannaqvi.rsvstudy.validator.ValidatorClass;
+
+public class Section04Activity extends AppCompatActivity {
+
+    ActivityF1Section04Binding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_f1_section04);
+        bi.setCallback(this);
+        this.setTitle("Form 01 (Case Reporting Form)");
+
+
+        /*bi.pocff03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == bi.pocff03a.getId())
+                    bi.pocff04.clearCheck();
+            }
+        });
+
+        bi.pocff06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == bi.pocff06a.getId())
+                    bi.pocff07.clearCheck();
+            }
+        });
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        try {
+            cal.setTime(sdf.parse(Section01Activity.DOB));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        DOBinMonths = DateUtils.ageInMonthsByDOB(cal);
+
+        if (DOBinMonths >= 6) {
+            bi.pocfh03cv.setVisibility(View.VISIBLE);
+        } else {
+            ClearClass.ClearAllFields(bi.pocfh03cv, null);
+            bi.pocfh03cv.setVisibility(View.GONE);
+        }
+
+        */
+
+    }
+
+    public void BtnContinue() {
+        if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                finish();
+                startActivity(new Intent(this, Section05Activity.class));
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+        int updcount = db.updateSD();
+
+        if (updcount == 1) {
+            Toast.makeText(this, "Updating Database... Successful!", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private void SaveDraft() throws JSONException {
+        JSONObject SD = new JSONObject();
+
+        SD.put("RS41", bi.RS41a.isChecked() ? "1"
+                : bi.RS41b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS421", bi.RS421a.isChecked() ? "1"
+                : bi.RS421b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS422", bi.RS422a.isChecked() ? "1"
+                : bi.RS422b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS423", bi.RS423a.isChecked() ? "1"
+                : bi.RS423b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS424", bi.RS424a.isChecked() ? "1"
+                : bi.RS424b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS4296", bi.RS4296a.isChecked() ? "1"
+                : bi.RS4296b.isChecked() ? "2"
+                : "0");
+        SD.put("RS4296ax", bi.RS4296ax.getText().toString());
+
+        SD.put("RS43", bi.RS43a.isChecked() ? "1"
+                : bi.RS43b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS441", bi.RS441a.isChecked() ? "1"
+                : bi.RS441b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS442", bi.RS442a.isChecked() ? "1"
+                : bi.RS442b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS443", bi.RS443a.isChecked() ? "1"
+                : bi.RS443b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS444", bi.RS444a.isChecked() ? "1"
+                : bi.RS444b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS445", bi.RS445a.isChecked() ? "1"
+                : bi.RS445b.isChecked() ? "2"
+                : "0");
+
+        SD.put("RS4496", bi.RS4496a.isChecked() ? "1"
+                : bi.RS4496b.isChecked() ? "2"
+                : "0");
+        SD.put("RS4496ax", bi.RS4496ax.getText().toString());
+
+        SD.put("RS45", bi.RS45a.isChecked() ? "1"
+                : bi.RS45b.isChecked() ? "2"
+                : bi.RS4598.isChecked() ? "98"
+                : "0");
+
+        SD.put("RS46", bi.RS46.getText().toString());
+
+        SD.put("RS47", bi.RS47.getText().toString());
+
+        SD.put("RS48", bi.RS48a.isChecked() ? "1"
+                : bi.RS48b.isChecked() ? "2"
+                : bi.RS4898.isChecked() ? "98"
+                : "0");
+
+
+        MainApp.fc.setsD(String.valueOf(SD));
+
+
+    }
+
+    private boolean formValidation() {
+        return ValidatorClass.EmptyCheckingContainer(this, bi.ll04);
+
+    }
+
+    public void BtnEnd() {
+        MainApp.endActivity(this, this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "You can't go back", Toast.LENGTH_SHORT).show();
+    }
+}
