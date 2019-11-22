22d041e4da2d2b0e723674cefdb510adbdc52360
hassan.naqvi <shznaqvi@gmail.com>
2019-11-22 14:14:42 +0500

    layout Styling

--- app/src/main/java/edu/aku/hassannaqvi/rsvstudy/contracts/FormsContract.java
+++ app/src/main/java/edu/aku/hassannaqvi/rsvstudy/contracts/FormsContract.java
@@ -12,7 +12,7 @@ import org.json.JSONObject;
 
 public class FormsContract {
 
-    private final String projectName = "RSV Study";
+    private final String projectName = "ANISA RSV Study";
     //private final String surveyType = "SN";
     private String _ID = "";
     private String _UID = "";
