8a159f92d7da382a365c1c82a5e455fda55ba3c3
SAJID LATIF <indus1530@gmail.com>
2020-01-07 11:25:29 +0500

    FORM2-B strings

---
+++ app/src/main/java/edu/aku/hassannaqvi/rsvstudy/ui/form1/Section06Activity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.rsvstudy.ui.form1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.rsvstudy.R;
+
+public class Section06Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f1_section06);
+    }
+}
