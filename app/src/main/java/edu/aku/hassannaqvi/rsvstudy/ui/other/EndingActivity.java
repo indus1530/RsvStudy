ee30d0e4cb72e83b94e248268eeba052b0882c87
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-06-10 11:06:58 +0500

    fixed RS88 question, skip and BtnContinue checked.

--- app/src/main/java/edu/aku/hassannaqvi/rsvstudy/ui/other/EndingActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/rsvstudy/ui/other/EndingActivity.java
@@ -29,7 +29,7 @@ public class EndingActivity extends AppCompatActivity {
         bi.setCallback(this);
 
 
-        Boolean check = getIntent().getExtras().getBoolean("complete");
+        boolean check = getIntent().getBooleanExtra("complete", false);
 
         if (check) {
             bi.istatus1.setEnabled(true);
